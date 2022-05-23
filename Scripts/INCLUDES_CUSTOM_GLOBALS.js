2c95ac45833c1ac0a6aa6080252fb22047f02ff1
lluaca <77507435+galvestonco@users.noreply.github.com>
2022-05-23 14:40:08 -0700

    Update INCLUDES_CUSTOM_GLOBALS.js
    to disable debug messages

--- Scripts/INCLUDES_CUSTOM_GLOBALS.js
+++ Scripts/INCLUDES_CUSTOM_GLOBALS.js
@@ -1,8 +1,2 @@
-showDebug = true; //was false changed for facility creation testing
+showDebug = false; //was false changed for facility creation testing
 showMessage = true; //was false  changed for facility creation testing 
-debugLevel = 3; //was 3  changed for facility creation testing
-if (currentUserID == "ADMIN") {
-   showDebug = true;
-   showMessage = true;
-   debugLevel = 3;
-}
